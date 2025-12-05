34ae929da5874daee5300aeb5479a05b4ac6a8a7
myxuchangbin <17875554+myxuchangbin@users.noreply.github.com>
2025-12-05 21:01:42 +0800

    Comment out tcp_notsent_lowat in sysctl config

--- startsys.sh
+++ startsys.sh
@@ -461,6 +461,7 @@ net.ipv4.tcp_fastopen = 3
 net.ipv4.tcp_mtu_probing = 1
 net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr
+#net.ipv4.tcp_notsent_lowat = 16384
 EOF
 echo -e "${yellow}→ 加载 sysctl 配置...${plain}"
 /sbin/sysctl -p /etc/sysctl.conf 2>/dev/null | awk -F' = ' '{printf "  ✔ %-40s = %s\n", $1, $2}'
